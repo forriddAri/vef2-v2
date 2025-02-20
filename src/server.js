abac8336558511e81dc0a107f4f4d33c7ec6de98
Ari Gunnar Kristjónsson <agk19@hi.is>
2025-02-20 13:44:04 +0000

    vonandi lagast

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import router from './routes.js';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 dotenv.config();
 
@@ -10,9 +12,10 @@ console.log('DB URL:', process.env.DATABASE_URL);
 
 const app = express();
 
-const viewsPath = 'views';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.set('views', viewsPath);
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
